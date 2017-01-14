17af34f87c0a241c65edfff0823122712a3cf064
Mark Kalinovits <mkalinovits@chemaxon.com>
2017-01-14 22:48:46 +0100

    separating first experiment

---
+++ src/main/js/hu/kalee/runner/ExperimentOne.ts
@@ -0,0 +1,43 @@
+/**
+ * Experiment with only procariotic cells.
+ */
+class ExperimentOne {
+    private runner: Runner;
+    private capacity: number = 50;
+    private hosts: Array<AgamicThing> = [];
+
+    public getCapacity(): number {
+        return this.capacity;
+    }
+
+    public getHosts(): Array<AgamicThing> {
+        return this.hosts;
+    }
+
+    constructor(selector: string, initial: number) {
+        for (let i = 0; i < initial; i++) {
+            let color = ExperimentOne.generateRandomColor();
+            this.hosts.push(new AgamicThing(color));
+        }
+        this.runner = new Runner(selector, this.capacity, this.hosts);
+    }
+
+    public run(): void {
+        this.runner.run();
+    }
+
+    public reset(): void {
+        this.runner.reset();
+    }
+
+    private static generateRandomColor(): string {
+        let red = ExperimentOne.randomEightBit().toString(16);
+        let green = ExperimentOne.randomEightBit().toString(16);
+        let blue = ExperimentOne.randomEightBit().toString(16);
+        return "#" + red + green + blue;
+    }
+
+    private static randomEightBit(): number {
+        return Math.floor(Math.random() * 255);
+    }
+}
