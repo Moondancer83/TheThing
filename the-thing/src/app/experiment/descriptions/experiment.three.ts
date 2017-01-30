7a8ee55e157d94586b23e7bbe7d07047811dc4a8
Mark Kalinovits <mkalinovits@chemaxon.com>
2017-01-30 09:03:28 +0100

    extracting experiment definitions (2)

--- the-thing/src/app/experiment/descriptions/experiment.three.ts
+++ the-thing/src/app/experiment/descriptions/experiment.three.ts
@@ -1,6 +1,17 @@
 import {Experiment} from "../experiment.class";
 import {Flask} from "../../environment/descriptions/flask";
+import {Host} from "../../host/host.interface";
+import {Plant} from "../../host/plant.class";
 
-let ExperimentThree = new Experiment('Experiment Three', 'Plants, herbivores and carnivores in a lab.', Flask, []);
+let plants: Host[] = [
+  new Plant(100, "#90EE90"),
+  new Plant(90,  "#32CD32")
+];
+let herbivores: Host[] = [];
+let carnivores: Host[] = [];
+
+let hosts: Host[] = [].concat(plants, herbivores, carnivores);
+
+let ExperimentThree = new Experiment('Experiment Three', 'Plants, herbivores and carnivores in a lab.', Flask, hosts);
 
 export { ExperimentThree };
--- the-thing/src/app/experiment/descriptions/experiment.two.ts
+++ the-thing/src/app/experiment/descriptions/experiment.two.ts
@@ -1,6 +1,16 @@
 import {Experiment} from "../experiment.class";
 import {Flask} from "../../environment/descriptions/flask";
+import {Host} from "../../host/host.interface";
+import {Plant} from "../../host/plant.class";
 
-let ExperimentTwo = new Experiment('Experiment Two', 'Plants and herbivores in a lab.', Flask, []);
+let plants: Host[] = [
+  new Plant(100, "#90EE90"),
+  new Plant(90,  "#32CD32")
+];
+let herbivores: Host[] = [];
+
+let hosts: Host[] = [].concat(plants, herbivores);
+
+let ExperimentTwo = new Experiment('Experiment Two', 'Plants and herbivores in a lab.', Flask, hosts);
 
 export { ExperimentTwo };
